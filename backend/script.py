cc0c88fd39e3f9345eb450d55da8e7c554279557
Sergio Damaceno Botelho <sergio.damaceno0002@gmail.com>
2022-06-13 15:05:21 -0300

    tratamento de erros

--- backend/script.py
+++ backend/script.py
@@ -9,51 +9,64 @@ def extrair(url):
         url = site + url + "/historical-data"
     if "currencies" in url:
         url = site + url + "-historical-data"
-
     if "indices" in url:
         url = site + url + "-historical-data"
+        
+    if not "indices" in url and not "currencies" in url and not "crypto" in url:
+        return "Erro URL invalida"
+    
     session = HTMLSession()
 
     resp = session.get(url)
 
     if "indices" in url:
-        indices_tags = resp.html.find("#curr_table")
-        indices_table = [tag.text for tag in indices_tags]
-        json_string = dumps(indices_table)
-        fechamento_indice = json_string[81:89]
-        if json_string[89] == "\\":
+        try:
+            indices_tags = resp.html.find("#curr_table")
+            indices_table = [tag.text for tag in indices_tags]
+            json_string = dumps(indices_table)
             fechamento_indice = json_string[81:89]
-        elif json_string[88] == "\\":
-            fechamento_indice = json_string[81:88]
-        elif json_string[87] == "\\":
-            fechamento_indice = json_string[81:87]
-        elif json_string[86] == "\\":
-            fechamento_indice = json_string[81:86]
+            if json_string[89] == "\\":
+                fechamento_indice = json_string[81:89]
+            elif json_string[88] == "\\":
+                fechamento_indice = json_string[81:88]
+            elif json_string[87] == "\\":
+                fechamento_indice = json_string[81:87]
+            elif json_string[86] == "\\":
+                fechamento_indice = json_string[81:86]
+        except:
+            return "Erro URL invalida"
 
         return fechamento_indice
 
     if "crypto" in url:
-        cryptos_tags = resp.html.find("#curr_table")
-        cryptos_table = [tag.text for tag in cryptos_tags]
-        json_string = dumps(cryptos_table)
-        fechamento_crypto = json_string[81:89]
-        if json_string[89] == "\\":
+        try:
+            cryptos_tags = resp.html.find("#curr_table")
+            cryptos_table = [tag.text for tag in cryptos_tags]
+            json_string = dumps(cryptos_table)
             fechamento_crypto = json_string[81:89]
-        elif json_string[88] == "\\":
-            fechamento_crypto = json_string[81:88]
-        elif json_string[87] == "\\":
-            fechamento_crypto = json_string[81:87]
-        elif json_string[86] == "\\":
-            fechamento_crypto = json_string[81:86]
+            if json_string[89] == "\\":
+                fechamento_crypto = json_string[81:89]
+            elif json_string[88] == "\\":
+                fechamento_crypto = json_string[81:88]
+            elif json_string[87] == "\\":
+                fechamento_crypto = json_string[81:87]
+            elif json_string[86] == "\\":
+                fechamento_crypto = json_string[81:86]
+        except:
+            return "Erro URL invalida"
 
         return fechamento_crypto
 
     if "currencies" in url:
-        currencies_tags = resp.html.find("#curr_table")
-        currencies_table = [tag.text for tag in currencies_tags]
-        json_string = dumps(currencies_table)
-        fechamento_currencies = json_string[75:81]
-
+        try:
+            currencies_tags = resp.html.find("#curr_table")
+            currencies_table = [tag.text for tag in currencies_tags]
+            json_string = dumps(currencies_table)
+            fechamento_currencies = json_string[75:81]
+            if fechamento_currencies == "":
+                raise
+        except:
+            return "Erro URL invalida"
         return fechamento_currencies
 
 def lendo_ativos():
@@ -143,3 +156,4 @@ def enviar_currencies():
                 spamwriter.writerows([resultado])
     return resultado
 
+print(extrair("crypto/lalalfdjkafs"))
